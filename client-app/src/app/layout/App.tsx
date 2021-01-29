73935ff05273d5a1803a3d27d8649c34cac6a032
ramon arents <ramonarents@hotmail.com>
2021-01-29 11:57:30 +0100

    End of section 5

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,79 @@
+import React, { useState, useEffect, Fragment } from "react";
+import axios from "axios";
+import { Container } from "semantic-ui-react";
+import { IActivity } from "../models/activity";
+import NavBar from "../../features/nav/NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
+    null
+  );
+
+  const [editMode, setEditMode] = useState(false); //by using booleans you don't have to tell the type
+
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.filter((a) => a.id === id)[0]);
+    setEditMode(false);
+  };
+
+  const handleOpenCreateForm = () => {
+    setSelectedActivity(null);
+    setEditMode(true);
+  };
+
+  const handleCreateActivity = (activity: IActivity) => {
+    setActivities([...activities, activity]);
+    setSelectedActivity(activity);
+    setEditMode(false);
+  };
+
+  const handleEditActivity = (activity: IActivity) => {
+    setActivities([
+      ...activities.filter((a) => a.id !== activity.id),
+      activity,
+    ]);
+    setSelectedActivity(activity);
+    setEditMode(false);
+  };
+
+  const handeDeleteActivity = (id: string) => {
+    setActivities([...activities.filter((a) => a.id !== id)]);
+  };
+
+  useEffect(() => {
+    axios
+      .get<IActivity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        let activities: IActivity[] = [];
+        response.data.forEach((activity) => {
+          activity.date = activity.date.split(".")[0];
+          activities.push(activity);
+        });
+        setActivities(activities);
+      });
+  }, []); //runs one time only. Removing the empty array will cause an infinite loop
+
+  return (
+    //you can use a fragment that is not displayed as element (in dev tools). This makes the code cleaner
+    <Fragment>
+      <NavBar openCreateForm={handleOpenCreateForm} />
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard
+          activities={activities}
+          selectActivity={handleSelectActivity}
+          selectedActivity={selectedActivity} //you can use ! after selectedActivity to avoid strong typing. It is better to declare the type in the interface of ActivityDashboard.
+          editMode={editMode}
+          setEditMode={setEditMode}
+          setSelectedActivity={setSelectedActivity}
+          createActivity={handleCreateActivity}
+          editActivity={handleEditActivity}
+          deleteActivity={handeDeleteActivity}
+        />
+      </Container>
+    </Fragment>
+  );
+};
+
+export default App;
